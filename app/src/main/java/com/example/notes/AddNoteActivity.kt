03cf45e1daf216396f99582314b9fc0fa6bee84c
rafael <santos_rafa1973@hotmail.com>
2023-09-04 20:25:41 -0300

    Adding database

--- app/src/main/java/com/example/notes/AddNoteActivity.kt
+++ app/src/main/java/com/example/notes/AddNoteActivity.kt
@@ -2,10 +2,29 @@ package com.example.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.notes.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.edTitle.text.toString()
+            val content = binding.edDescription.text.toString()
+            val note = Note(0, title, content)
+
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
