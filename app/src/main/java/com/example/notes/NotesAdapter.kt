270d43cb90ad5edcf6222a79dbeeb071867e9548
rafsn <santos_rafa1973@hotmail.com>
2023-09-14 20:35:29 -0300

    Add items and adapter

---
+++ app/src/main/java/com/example/notes/NotesAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.notes
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter(private var notes: List<Note>, context: Context) :
+    RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.tvTitle)
+        val content: TextView = itemView.findViewById(R.id.tvContent)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.title.text = note.title
+        holder.content.text = note.content
+    }
+
+    fun refreshData(newNotes: List<Note>) {
+        notes = newNotes
+        notifyDataSetChanged()
+    }
+}
